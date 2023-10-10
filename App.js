563c9e9082554b314d38cf6b32d6548d029418be
nhattranhung <maonhattinhquan2312@gmail.com>
2023-10-10 15:11:26 +0700

    test

---
+++ App.js
@@ -0,0 +1,189 @@
+import React, {useState} from 'react';
+// import { StatusBar } from 'expo-status-bar';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView, StatusBar, Appearance } from 'react-native';
+import Task from './components/Task';
+
+export default function App() {
+  
+  StatusBar.setBarStyle('light-content', true);
+
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+  const [strikeThrough, setStrikeThrough] = useState(false);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+
+  }
+
+  const deleteTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
+  const toggleStrikeThrough = (index) => {
+    setStrikeThrough(!strikeThrough);
+  };
+
+
+  return (
+    <View style={styles.container}>
+
+      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+      <ScrollView contentContainerStyle={{flexGrow: 1}} keyboardShouldPersistTaps='handled'>
+
+      {/* Todo App */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>To-Do App</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity
+                style = {
+                    strikeThrough ? styles.strikeThroughTask : styles.normalTask
+                }
+                key={index}
+                onPress={() => toggleStrikeThrough(index)}
+                onLongPress={() => deleteTask(index)}>
+                  <Task key={index} text={item} />
+                </TouchableOpacity>
+
+              )
+            })
+          }
+
+        </View>
+      </View>
+      </ScrollView>
+      
+      {/* Write a task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Add a task'}
+          value={task} onChangeText={text => setTask(text) } 
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
+    </View>
+  );
+}
+
+//Colors for the themes
+const darkMode = {
+  backgroundColor: '#1D3557',
+  primaryColor: '#F1FAEE',
+  secondaryColor: '#A8DADC',
+  accentColor: '#E63946',
+};
+
+const lightMode = {
+  backgroundColor: '#F1FAEE',
+  primaryColor: '#457B9D',
+  secondaryColor: '#1D3557',
+  accentColor: '#E63946',
+};
+
+// Find out what the current theme is using appearance
+
+let theme = Appearance.getColorScheme();
+if (theme === 'dark') {
+  currentTheme = darkMode;
+  console.log('Dark mode is on');
+}
+else {
+  currentTheme = lightMode;
+  console.log('Light mode is on');
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: currentTheme.backgroundColor,
+  },
+
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingBottom: 10,
+    color: theme.primaryColor,
+  },
+
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 40,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: '80%',
+    height: 60,
+    backgroundColor: currentTheme.primaryColor,
+    borderRadius: 60,
+    borderColor: currentTheme.secondaryColor,
+    borderWidth: 1,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 7,},
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+    elevation: 14,
+  },
+
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: currentTheme.accentColor,
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 7,},
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+    elevation: 14,
+  },
+
+  addText: {
+    fontSize: 30,
+    color: currentTheme.primaryColor,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+
+  strikeThroughTask: {
+    fontSize: 20,
+    textDecorationLine: "line-through",
+    color: "red", 
+  },
+
+  normalTask: {
+    fontSize: 20,
+    color: "blue",
+  },
+
+});
---
+++ README.md
@@ -0,0 +1,29 @@
+# To Do App
+
+## Made with React Native
+
+### Tools
+
+Built using the following;
+- [React Native](https://facebook.github.io/react-native/)
+- [Expo](https://expo.io/)
+
+I also used [this tutorial](https://www.youtube.com/watch?v=0kL6nhutjQ8) by [Matt Frances](https://github.com/mattfrances) as a start point.
+
+### Setup
+
+The project at this stage can only be run in a local enviroment using Expo.
+
+To run the project locally, you will need to have Expo installed on your machine. You can find the installation instructions [here](https://docs.expo.io/versions/latest/introduction/installation.html).
+
+Once you have Expo installed, you can run the project by running the following command in the root directory of the project.
+
+```bash
+expo start
+```
+
+Then you can run the project on your local machine using the Expo app on your phone or using an emulator.
+
+### Screenshots
+
+<img src="/assets/Screenshot1.png"  width="40%" height="40%" alt="Screenshot">
